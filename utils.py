856273511bcdc5dc8d9356d268f1b3dfca208443
lebedev-ilia <150586009+lebedev-ilia@users.noreply.github.com>
2025-04-13 15:41:41 +1000

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import torch
 
 
 class BertInputItem(object):
-    """An item with all the necessary attributes for finetuning BERT."""
 
     def __init__(self, text, input_ids, input_mask, segment_ids, label_id):
         self.text = text
@@ -13,25 +12,19 @@ class BertInputItem(object):
 
 
 def convert_examples_to_inputs(example_texts, example_labels, label2idx, max_seq_length, tokenizer):
-    """Loads a data file into a list of `InputBatch`s."""
     
     input_items = []
     examples = zip(example_texts, example_labels)
     for (ex_index, (text, label)) in enumerate(examples):
 
-        # Create a list of token ids
         input_ids = tokenizer.encode(f"[CLS] {text} [SEP]")
         if len(input_ids) > max_seq_length:
             input_ids = input_ids[:max_seq_length]
 
-        # All our tokens are in the first input segment (id 0).
         segment_ids = [0] * len(input_ids)
 
-        # The mask has 1 for real tokens and 0 for padding tokens. Only real
-        # tokens are attended to.
         input_mask = [1] * len(input_ids)
 
-        # Zero-pad up to the sequence length.
         padding = [0] * (max_seq_length - len(input_ids))
         input_ids += padding
         input_mask += padding
@@ -59,14 +52,10 @@ def convert_example_to_input_infer(text, max_seq_length, tokenizer):
     if len(input_ids) > max_seq_length:
         input_ids = input_ids[:max_seq_length]
 
-    # All our tokens are in the first input segment (id 0).
     segment_ids = [0] * len(input_ids)
 
-    # The mask has 1 for real tokens and 0 for padding tokens. Only real
-    # tokens are attended to.
     input_mask = [1] * len(input_ids)
 
-    # Zero-pad up to the sequence length.
     padding = [0] * (max_seq_length - len(input_ids))
     input_ids += padding
     input_mask += padding
